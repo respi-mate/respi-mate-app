23bd594fa998a9c117200d37eeb8f046d74c4f11
Aishwarya Pattar <ajp15@ic.ac.uk>
2019-03-09 12:46:59 +0000

    Created views and linked them to view controllers

--- Respi-mate/HistoryViewController.swift
+++ Respi-mate/HistoryViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HistoryViewController.swift
 //  Respi-mate
 //
 //  Created by Aishwarya Pattar on 09/03/2019.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HistoryViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
 }
-
---
+++ Respi-mate/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  Respi-mate
+//
+//  Created by Aishwarya Pattar on 09/03/2019.
+//  Copyright © 2019 Aishwarya Pattar. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
